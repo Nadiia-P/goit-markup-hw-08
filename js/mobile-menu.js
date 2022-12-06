59b324ed3e3bde8bc57178f1b9610da0cd7c74c5
Nadiia P <nadiia.parakhina@gmail.com>
2022-12-06 20:14:21 +0200

    HW done

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,12 +1,13 @@
 (() => {
   const menuBtn = document.querySelector('[data-menu-button]');
   const mobileMenu = document.querySelector('[data-menu]');
-
+  const body = document.querySelector('body');
   menuBtn.addEventListener('click', () => {
     const expanded = menuBtn.getAttribute('aria-expanded') === 'true' || false;
     menuBtn.classList.toggle('is-open');
     menuBtn.setAttribute('aria-expanded', !expanded);
     mobileMenu.classList.toggle('is-open');
+    body.classList.toggle('no-scroll');
   });
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
